3519d1c5f40df27c4a51dd39894296a6e66caccc
Vanja Suhina <vanja@prehnit.hr>
2018-12-03 16:26:47 +0100

    Day01 optimization

--- 01/0101.py
+++ 01/0101.py
@@ -1,12 +1,4 @@
 with open("input.txt") as f:
     content = f.readlines()
 
-freq = 0
-
-for line in content:
-    if line[0] == '+':
-        freq += int(line[1:])
-    elif line[0] == '-':
-        freq -= int(line[1:])
-
-print(freq)
+print(sum(map(int,content)))
--- 01/0102.py
+++ 01/0102.py
@@ -2,20 +2,15 @@ with open("input.txt") as f:
     content = f.readlines()
 
 freq = 0
-freqlist = [0]
+freqset = set([0])
 found = False
 
 while not(found):
     for line in content:
-        if line[0] == '+':
-            freq += int(line[1:])
-        elif line[0] == '-':
-            freq -= int(line[1:])
-        if freq in freqlist:
+        freq += int(line)
+        if freq in freqset:
             print(freq)
             found = True
             break
         else:
-            freqlist.append(freq)
-
-print('end')
+            freqset.add(freq)
